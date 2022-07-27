ccf19f85656285a6481acae5968d996f3d61db9d
Anthon Hall <anthon.hall@gmail.com>
2022-07-27 14:05:59 -0600

    PowerShell Examples Upload
    feat:  add example scripts

---
+++ Detect Camera Network Disabled and Restart.ps1
@@ -0,0 +1,12 @@
+﻿$Adapter = Get-WmiObject -Class Win32_NetworkAdapter  -Filter "NetConnectionID LIKE 'CameraNetwork' and NetEnabled=false"
+if($Adapter){
+    c:
+    cd '\Program Files\DevManView'
+    .\DevManView.exe /disable_enable "Intel(R) Gigabit CT Desktop Adapter"
+    $To         = "Warehouse Systems <REDACTED@overstock.com>";
+    $From       = "Warehouse Systems <REDACTED@overstock.com>";
+    $SmtpServer = "REDACTED.overstock.com";
+    $Subject    = "REDACTED Network Adapter Enabled";
+    $Results    = "The CameraNetwork network adapter on REDACTED has been enabled. If this continues to happen every 5 minutes, disable this Windows Scheduler Task and investigate further.";
+    Send-MailMessage -smtpserver $SmtpServer -Priority High -To $To -From $From -Body $Results -Subject $Subject -BodyAsHtml
+}
---
+++ QuickBooks SalesReceipt Scraper.ps1
@@ -0,0 +1,53 @@
+﻿<#Usage
+-all Exports all data
+-startDate Exports data after this date
+-endDate Exports data before this date
+#>
+
+param (
+    [switch] $all,
+    [datetime] $startDate,
+    [datetime] $endDate
+)
+
+#Windows Authentication
+$connectstring = "DSN=QuickBooks POS Data;OLE DB Services=-2;"
+$sql = "select * from SalesReceipt"
+
+$conn = New-Object System.Data.Odbc.OdbcConnection($connectstring)
+$conn.Open()
+$cmd = New-Object system.Data.Odbc.OdbcCommand($sql,$conn)
+$da = New-Object system.Data.Odbc.OdbcDataAdapter($cmd)
+$dt = New-Object system.Data.datatable
+$null = $da.fill($dt)
+$conn.close()
+$date = Get-Date -UFormat "%m%d%Y"
+
+
+If ($all) {
+    #All data
+    $filename = "\\overstock.com\REDACTED\SalesReceipt_" + $date + "_All.csv"
+    $dt | Export-Csv $filename -notype
+    }
+Else {
+    If (!$startDate -and !$endDate) {
+        #Yesterday's data
+        $filename = "\\overstock.com\REDACTED\SalesReceipt_" + $date + ".csv"
+        $dt | Where TxnDate -gt (Get-Date).AddDays(-2) | Export-Csv $filename -notype
+        }
+    Else {
+        #Specified date range
+        If (!$startDate) {$startDate = (Get-Date -month 01 -day 01 -year 1970)}
+        If (!$endDate) {$endDate = (Get-Date).AddDays(1)}
+        If ($startDate -gt $endDate) {
+            $tempDate = $startDate
+            $startDate = $endDate
+            $endDate = $tempDate
+            }
+        $startDateDOS = Get-Date $startDate -UFormat "%m%d%Y"
+        $endDateDOS = Get-Date $endDate -UFormat "%m%d%Y"
+        $filename = "\\overstock.com\REDACTED\SalesReceipt_" + $startDateDOS + "-" + $endDateDOS + ".csv"
+        Write-Host $filename
+        $dt | where TxnDate -gt $startDate | Where TxnDate -lt $endDate | Export-Csv $filename -notype
+        }
+    }
